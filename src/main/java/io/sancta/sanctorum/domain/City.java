4ce103aaaf6e47bc5ddd828f6c1da1e962f78154
HeVetal <heavyve@gmail.com>
2024-10-10 15:42:05 +0300

    add Annotation and field

--- src/main/java/io/sancta/sanctorum/domain/City.java
+++ src/main/java/io/sancta/sanctorum/domain/City.java
@@ -1,4 +1,20 @@
 package io.sancta.sanctorum.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema = "world", name = "city")
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String name;
+
+    @JoinColumn(name = "country_id")
+    Country country;
+
+    String district;
+
+    Integer population;
 }
--- src/main/java/io/sancta/sanctorum/domain/Country.java
+++ src/main/java/io/sancta/sanctorum/domain/Country.java
@@ -1,4 +1,15 @@
 package io.sancta.sanctorum.domain;
 
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(schema = "world", name = "country")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
 }
