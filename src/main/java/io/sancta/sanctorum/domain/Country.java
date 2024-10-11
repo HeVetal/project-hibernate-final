2a2db0da3051242036605abfb8f6cb01510ce42c
HeVetal <heavyve@gmail.com>
2024-10-11 15:03:22 +0300

    add Fields

--- src/main/java/io/sancta/sanctorum/domain/Country.java
+++ src/main/java/io/sancta/sanctorum/domain/Country.java
@@ -5,11 +5,52 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(schema = "world", name = "country")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Country {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    String code;
+
+    @Column(name = "code_2")
+    String alternativeCode;
+
+    String name;
+
+    Continent continent;
+
+    String region;
+
+    @Column(name = "surface_area")
+    BigDecimal surfaceArea;
+
+    @Column(name = "indep_year")
+    Short independenceYear;
+
+    Integer population;
+
+    @Column(name = "life_expectancy")
+    BigDecimal lifeExpectancy;
+
+    BigDecimal GNP;
+
+    @Column(name = "gnpo_id")
+    BigDecimal GNPOId;
+
+    @Column(name = "local_name")
+    String localName;
+
+    @Column(name = "government_from")
+    String governmentFrom;
+
+    @Column(name = "head_of_state")
+    String headOfState;
+
+    @OneToOne
+    @Column(name = "capital")
+    City city;
 }
