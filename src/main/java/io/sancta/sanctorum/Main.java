25d60b3d580c19d2e291145e5959f8556b0d72a2
HeVetal <heavyve@gmail.com>
2024-10-17 15:27:23 +0300

    add Fields

--- src/main/java/io/sancta/sanctorum/Main.java
+++ src/main/java/io/sancta/sanctorum/Main.java
@@ -1,8 +1,22 @@
 package io.sancta.sanctorum;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.lettuce.core.RedisClient;
+import io.sancta.sanctorum.dao.CityDAO;
+import io.sancta.sanctorum.dao.CountryDAO;
+import org.hibernate.SessionFactory;
+
 public class Main {
 
+    private SessionFactory sessionFactory;
+    private RedisClient redisClient;
+    private ObjectMapper objectMapper;
+    private CityDAO cityDAO;
+    private CountryDAO countryDAO;
+
+
     public Main() {
+
     }
 
     public static void main(String[] args) {
