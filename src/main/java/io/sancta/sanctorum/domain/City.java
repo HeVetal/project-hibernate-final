72741db5e2ef2824b57c9c2f07696087c34f31fe
HeVetal <heavyve@gmail.com>
2024-10-17 15:26:10 +0300

    add Annotation

--- src/main/java/io/sancta/sanctorum/domain/City.java
+++ src/main/java/io/sancta/sanctorum/domain/City.java
@@ -1,9 +1,16 @@
 package io.sancta.sanctorum.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
+@Getter
+@Setter
 @Entity
 @Table(schema = "world", name = "city")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class City {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
