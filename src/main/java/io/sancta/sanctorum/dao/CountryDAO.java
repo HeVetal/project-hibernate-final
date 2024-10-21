09185d8938b41037c6ef9abac617cfb8addc2e42
HeVetal <heavyve@gmail.com>
2024-10-21 16:06:56 +0300

    add CountryDAO

--- src/main/java/io/sancta/sanctorum/dao/CountryDAO.java
+++ src/main/java/io/sancta/sanctorum/dao/CountryDAO.java
@@ -1,4 +1,22 @@
 package io.sancta.sanctorum.dao;
 
+import io.sancta.sanctorum.domain.Country;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor
 public class CountryDAO {
+    SessionFactory sessionFactory;
+
+    public List<Country> getAll(){
+        String hql = "select c from Country as c";
+        Query<Country> query = sessionFactory.getCurrentSession().createQuery(hql, Country.class);
+        return query.list();
+    }
 }
